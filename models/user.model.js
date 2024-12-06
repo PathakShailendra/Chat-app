2b9d25a0d0139a0f7e9495cad15c2ddeebff87e8
Shailendra Pathak <pathakharsh813@gmail.com>
2024-12-06 23:59:05 +0530

    first commit

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    googleId: {
+      type: String,
+      required: true,
+    },
+    displayName: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    socketId: {
+      type: String,
+      default: "",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
